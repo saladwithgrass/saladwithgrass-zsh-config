02b9f792930f05519336f9d4a36921b26a653e95
jazzhands <egod09080908@yandex.ru>
2025-05-18 09:51:29 +0300

    added setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# update submodules
+git submodule update --remote --init --recursive
+
+# backup .zshrc
+BACKUP_FILE=~/.zshrc.prev
+echo "Backing up your .zshrc"
+echo "It will be saved to $BACKUP_FILE"
+mv ~/.zshrc $BACKUP_FILE
+
+# symlink to .zshrc
+echo "Creating symlink to .zshrc"
+ln -s $PWD/.zshrc ~/.zshrc
